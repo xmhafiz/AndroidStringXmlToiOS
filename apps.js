e422903ec11940c3eb53c06291c813b0ecc6044f
Mohd Hafiz <xmhafiz@gmail.com>
2020-10-15 02:03:45 +0800

    Added function to write into formatted iOS localizable file

--- apps.js
+++ apps.js
@@ -13,19 +13,35 @@ fs.readFile('./strings.xml', 'utf8', (err, data) => {
         console.error(err);
         return
     }
-
+    // put all xml into long string
     let xml = data.toString();
     parseString(xml, function (err, res) {
-
-        
-        // let str = res.replace('/\"-\"/g', '\"key\"')
+        // map strings values into array of kvo
         let stringArray = res.resources.string;
         let kvoArray = stringArray.map(getKvo);
-        console.log(kvoArray);
+        writeLocalizeFile(kvoArray);
     })
 });
 
 function getKvo(item) {
     let obj = new Kvo(item.$.name, item._)
     return obj;
+}
+
+function writeLocalizeFile(arr) {
+    // create a content
+    var content = '';
+    arr.forEach(el => {
+        content += `"${el.key}"=` + `"${el.value}"\n`;
+    });
+    // content to be written into file
+    console.log(content);
+    // then write to files
+    fs.writeFile('./Localizable.strings', content, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log('Bravo, file written successfully 🎉');
+      })
 }
